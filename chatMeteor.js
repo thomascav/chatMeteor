cf56302806365e803b3f115ce84c83421a7ad309
thomascav <tcavalar@numericable.fr>
2016-03-01 13:13:03 +0100

    changed html => messages template ; changed js => template messages

--- chatMeteor.js
+++ chatMeteor.js
@@ -1,17 +1,9 @@
+Messages = new Mongo.Collection('messages');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    Template.messages.helpers({
+    messages: function () {
+      return Messages.find();
     }
   });
 }
