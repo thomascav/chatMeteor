88a31c828fc6585712ba47d15fcd8984ca9688ef
thomascav <tcavalar@numericable.fr>
2016-03-01 14:03:30 +0100

    added moment:js package => time on messages

--- chatMeteor.js
+++ chatMeteor.js
@@ -6,7 +6,7 @@ if (Meteor.isClient) {
       return Messages.find({});
     }
   });
-}
+
 Template.messages.events({
     'keypress textarea' : function(e) {
         if (e.keyCode == 13) {//enter key pressed
@@ -14,14 +14,29 @@ Template.messages.events({
             instance.find('textarea').value = '';
 
             Messages.insert ({
-                messa:value,
-                timestamp: new Date ()
+                message:value,
+                timestamp: new Date(),
                 // user: Meteor.userId()
             });
         }
     }
-})
+});
+
+Template.message.helpers({
+    user: function() {
+            return Meteor.users.findOne({_id: this.user});
+    },
+    time: function() {
+        return (this.timestamp).format('h:mm a');
+    }
+});
+
+Accounts.config({
+    passwordSingupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+});
+
 }
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
