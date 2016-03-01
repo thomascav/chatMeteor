9b98d9f8d30239a4751f6f00ffe1c217f63b1ad7
thomascav <tcavalar@numericable.fr>
2016-03-01 13:24:34 +0100

    added textarea in js

--- chatMeteor.js
+++ chatMeteor.js
@@ -7,7 +7,14 @@ if (Meteor.isClient) {
     }
   });
 }
+Template.messages.events({
+    'keypress textarea' : function(e) {
+        if (e.keyCode == 13) {//enter key pressed
+            var value = instance.find('textarea').value;
 
+        }
+    }
+})
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
